e9312959b60cc62e8769da0ce5fee13c532dbe54
Jamsheelamk <jamssheelamk@gmail.com>
2022-02-22 13:15:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogoComponent } from './logo/logo.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css']
+})
+export class LogoComponent implements OnInit {
+ 
+  constructor() { }
+
+
+  windowWidth?: string;
+  showSplash = true;
+
+
+  ngOnInit(): void {
+   
+    setTimeout(() => {
+      this.windowWidth = "-" + window.innerWidth + "px";
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, 500);
+    }, 3000);
+  }
+  }
